e1878c56d62c77f7a553a816b21546a65e7ca2ae
MahmoudYoussef <83463726+Garuooo@users.noreply.github.com>
2023-12-25 11:28:43 +0200

    Add files via upload

---
+++ docker/broker-test/dependents.sh
@@ -0,0 +1,2 @@
+wget https://repo1.maven.org/maven2/io/prometheus/jmx/jmx_prometheus_javaagent/0.3.0/jmx_prometheus_javaagent-0.3.0.jar
+sudo wget https://downloads.apache.org/kafka/3.5.1/kafka_2.13-3.5.1.tgz
---
+++ docker/zookeeper-test/dependents.sh
@@ -0,0 +1,3 @@
+sudo wget https://archive.apache.org/dist/zookeeper/zookeeper-3.9.0/apache-zookeeper-3.9.0-bin.tar.gz
+sudo wget https://downloads.apache.org/kafka/3.5.1/kafka_2.13-3.5.1.tgz
+wget https://repo1.maven.org/maven2/io/prometheus/jmx/jmx_prometheus_javaagent/0.3.0/jmx_prometheus_javaagent-0.3.0.jar
